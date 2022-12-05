ec2e7295f1f6c7cbff08dbcfd333cf54be2d4433
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-05 13:27:08 +0530

    create required models for the app
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Wallet, Transaction, Label, User
 
 # Register your models here.
+admin.register(Wallet, Transaction, Label, User)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Wallet(models.Model):
+    name = models.CharField("Name of the Wallet", null=False, max_length=50)
+    description = models.TextField("Description of the Wallet", default="")
+    created_on = models.DateTimeField("Created On", auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    balance = models.IntegerField("Current Balance", default=0)
+
+class Label(models.Model):
+    name = models.CharField("Name of the Label", null=False, max_length=50)
+    description = models.TextField("Description of the Label", default="")
+    created_on = models.DateTimeField("Created On", auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    color = models.CharField("Color of the Label (in hex format)", max_length=7)
+
+class Transaction(models.Model):
+    amount = models.IntegerField("Transaction Amount", default=0)
+    is_expense = models.BooleanField("Is it an expense?", default=True)
+    labels = models.ManyToManyField(Label, verbose_name="Labels")
+    wallet = models.ForeignKey(Wallet, verbose_name="Wallet Used for Transaction", on_delete=models.CASCADE)
+
+    date_time = models.DateTimeField("Date & Time of Transaction", auto_now_add=True)
+
+    # These will be used to crete data rich statistics for the user
+    day = models.IntegerField("Date of the Month")
+    week = models.IntegerField("Week of the Month")
+    month = models.IntegerField("Month Of the Year")
+    year = models.IntegerField("Year of Transaction")
