ed08893ff50850d18225f4e631a44653f30a4a37
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-10 16:26:40 +0530

    send minimal wallet and label data along with transactions
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- api/serializers.py
+++ api/serializers.py
@@ -2,22 +2,49 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Wallet, Transaction, Label
 
+
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
         fields = "__all__"
+
 
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
         fields = "__all__"
 
+
+class WalletSerializerMinimal(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ["id", "name"]
+
+
+class LabelSerializerMinimal(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ["id", "name"]
+
+
 class TransactionSerializer(serializers.ModelSerializer):
+    labels = LabelSerializerMinimal(many=True)
+    wallet = WalletSerializerMinimal()
+
     class Meta:
         model = Transaction
-        fields = "__all__"
+        fields = [
+            "name",
+            "description",
+            "amount",
+            "is_expense",
+            "labels",
+            "wallet",
+            "date_time"]
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "first_name", "last_name", "date_joined"]
+        fields = ["username", "email", "first_name",
+                  "last_name", "date_joined"]
